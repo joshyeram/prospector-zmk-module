f0fdf55328476f544fcae71a05b381f5c1a06a73
joshyeram <joshyc423@gmail.com>
2025-06-02 00:08:58 -0400

    testing scale

--- boards/shields/prospector_adapter/src/custom_status_screen.c
+++ boards/shields/prospector_adapter/src/custom_status_screen.c
@@ -16,10 +16,8 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-//static struct zmk_widget_layer_roller layer_roller_widget;
 static struct zmk_widget_layer_prndl layer_prndl_widget;
 static struct zmk_widget_battery_bar battery_bar_widget;
-//static struct zmk_widget_caps_word_indicator caps_word_indicator_widget;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
@@ -27,18 +25,11 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_bg_color(screen, lv_color_hex(0x000000), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(screen, 255, LV_PART_MAIN);
 
-    //zmk_widget_caps_word_indicator_init(&caps_word_indicator_widget, screen);
-    //lv_obj_align(zmk_widget_caps_word_indicator_obj(&caps_word_indicator_widget), LV_ALIGN_RIGHT_MID, -10, 46);
-
     zmk_widget_battery_bar_init(&battery_bar_widget, screen);
     lv_obj_set_size(zmk_widget_battery_bar_obj(&battery_bar_widget), lv_pct(100), 48);
     lv_obj_align(zmk_widget_battery_bar_obj(&battery_bar_widget), LV_ALIGN_BOTTOM_MID, 0, 0);
 
-    //zmk_widget_layer_roller_init(&layer_roller_widget, screen);
-    //lv_obj_set_size(zmk_widget_layer_roller_obj(&layer_roller_widget), 224, 140);
-    //lv_obj_align(zmk_widget_layer_roller_obj(&layer_roller_widget), LV_ALIGN_CENTER, 0, -20);
-
     zmk_widget_layer_prndl_init(&layer_prndl_widget, screen);
-    
+
     return screen;
 }
