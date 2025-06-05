30575b48bc7a763e4fc634d03e59ba3ff66508ab
joshyeram <joshyc423@gmail.com>
2025-06-04 23:03:10 -0400

    cosmetic

--- boards/shields/prospector_adapter/src/widgets/wpm_ind.c
+++ boards/shields/prospector_adapter/src/widgets/wpm_ind.c
@@ -47,11 +47,12 @@ int zmk_widget_wpm_ind_init(struct zmk_widget_wpm_ind *widget, lv_obj_t *parent)
 {
     widget->obj = lv_meter_create(parent);
     lv_obj_set_size(widget->obj, 175, 175);
-    lv_obj_align(widget->obj, LV_ALIGN_CENTER, 0, -55);
+    lv_obj_align(widget->obj, LV_ALIGN_CENTER, 0, -25);
     lv_meter_scale_t * scale = lv_meter_add_scale(widget->obj);
 
-    lv_meter_set_scale_ticks(widget->obj, scale, 41, 1, 3, lv_color_white());
-    lv_meter_set_scale_major_ticks(widget->obj, scale, 8, 3, 6, lv_color_white(), 10);
+    lv_obj_set_style_text_color(widget->obj, lv_color_white(), LV_PART_TICK);
+    lv_meter_set_scale_ticks(widget->obj, scale, 41, 2, 4, lv_color_white());
+    lv_meter_set_scale_major_ticks(widget->obj, scale, 8, 5, 10, lv_color_white(), 10);
     lv_meter_set_scale_range(widget->obj, scale, 0, 200, 240, 150);
 
     /*Add a white arc to the red*/
