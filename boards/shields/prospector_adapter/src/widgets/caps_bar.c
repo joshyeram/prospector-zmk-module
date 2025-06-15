715c911bc1912c0e738c0e5370e4efb147a333fc
joshyeram <joshyc423@gmail.com>
2025-06-15 15:16:35 -0400

    show

--- boards/shields/prospector_adapter/src/widgets/caps_bar.c
+++ boards/shields/prospector_adapter/src/widgets/caps_bar.c
@@ -33,13 +33,13 @@ static void caps_bar_set(lv_obj_t *bar, struct caps_bar_state state)
     if (state.ind & LED_CLCK) 
     {
         LOG_INF("clck");
-        lv_obj_clear_flag(bar, LV_OBJ_FLAG_HIDDEN);
+        //lv_obj_clear_flag(bar, LV_OBJ_FLAG_HIDDEN);
         lastActive = true;
     }
     else
     {
         LOG_INF("no clck");
-        lv_obj_add_flag(bar, LV_OBJ_FLAG_HIDDEN);
+        //lv_obj_add_flag(bar, LV_OBJ_FLAG_HIDDEN);
         lastActive = false;
     }
 }
@@ -65,11 +65,11 @@ static void caps_bar_toggle_sel(lv_obj_t *meter, struct caps_bar_visual_state st
 {   
     if(state.act == ZMK_ACTIVITY_IDLE || state.act == ZMK_ACTIVITY_SLEEP)
     {
-        lv_obj_add_flag(meter, LV_OBJ_FLAG_HIDDEN);
+        //lv_obj_add_flag(meter, LV_OBJ_FLAG_HIDDEN);
     }   
     else if (state.act == ZMK_ACTIVITY_ACTIVE && lastActive)
     {
-        lv_obj_clear_flag(meter, LV_OBJ_FLAG_HIDDEN);
+        //lv_obj_clear_flag(meter, LV_OBJ_FLAG_HIDDEN);
     }
 }
 
